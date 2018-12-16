7f7b0361854483693b3a0fbbd3ddd4fff35c7e07
andresrgallo <andresgallo@hotmail.com>
2018-12-16 11:27:27 +1100

    Fix media queries for small phones

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,10 +4,12 @@ import './css/Home.css';
 export default function Home() {
 	return (
 		<div className="home-container">
-			<h1 className="hero">
-				<ion-icon name="home" class="home-icon" />
-			</h1>
-			<h2 id="home-line">Property Managers</h2>
+			<div className="h2-icon">
+				<h1 className="hero">
+					<ion-icon name="home" class="home-icon" />
+				</h1>
+				<h2 id="home-line">Property Managers</h2>
+			</div>
 		</div>
 	);
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './css/NavBar.css';
 
 export default function NavBar() {
-  return (
-    <div className="menuBar ">
-      <div className="container">
-        <ul>
-          <li className="logo">
-            <NavLink exact activeClassName="active" to="/">
-              <ion-icon name="home" className="icon" />
-              <span>Unique Rentals</span>
-            </NavLink>
-          </li>
-          <li className="tenants">
-            <NavLink activeClassName="active" to="/leases">
-              Tenants List
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="menuBar ">
+			<div className="container">
+				<ul id="navbar-links">
+					<li className="logo">
+						<NavLink exact activeClassName="active" to="/">
+							<ion-icon name="home" className="icon" />
+							<span>Unique Rentals</span>
+						</NavLink>
+					</li>
+					<li className="tenants">
+						<NavLink activeClassName="active" to="/leases">
+							Tenants List
+						</NavLink>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
